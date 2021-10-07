f0f3b3e18dec0f04d736730ed968a794c92c37ac
LinconViana <linconviana@gmail.com>
2021-10-06 22:15:49 -0300

    criando operacao de instancicao

--- src/main/java/com/linconviana/projeto01/Projeto01Application.java
+++ src/main/java/com/linconviana/projeto01/Projeto01Application.java
@@ -1,13 +1,29 @@
 package com.linconviana.projeto01;
 
+import com.linconviana.projeto01.domain.Categoria;
+import com.linconviana.projeto01.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Projeto01Application {
+public class Projeto01Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto01Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
